2cba7fcc16b5cf24adc669452e2fdff1460e47fa
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-03-26 12:07:50 -0600

    Section 2 complete

--- tny_script.js
+++ tny_script.js
@@ -10,5 +10,17 @@
    Date:   26/03/2023
 
 */
+/* Store the current date and time */
+var currentDay = new Date("May 23, 2021 14:35:05");
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
 
 window.alert("Welcome to Tulsa");
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML =
+dateStr + "<br />" + timeStr;
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
