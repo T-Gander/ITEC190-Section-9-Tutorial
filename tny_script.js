e64a2c6c3ebb1b87a10c3b2ef52b20ea22ddde8c
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-03-26 11:57:09 -0600

    Section 1 complete

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,14 @@
+"use strict"
+
 /*
    New Perspectives on HTML5 and CSS3, 8th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Thomas Gander
+   Date:   26/03/2023
 
 */
 
+window.alert("Welcome to Tulsa");
